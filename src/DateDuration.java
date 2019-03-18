5016f3201be478726bf9ce12ad8d93512995c688
Paramjit Singh <36759323+Paramjit21@users.noreply.github.com>
2019-03-18 16:37:06 +0530

    Add files via upload

--- src/DateDuration.java
+++ src/DateDuration.java
@@ -8,15 +8,27 @@ public class DateDuration {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String input;
-		System.out.print("Enter date: ");
 		Scanner sc = new Scanner(System.in);
-		input = sc.next();
-		Date d = new Date();
-		long l = (long)Date.parse(input);
-		Date newD = new Date(l);
-		int du = d.compareTo(newD); 
-		System.out.println("Duration with '"+d+"' is "+du);
-		sc.close();
+		while(true)
+		{
+			System.out.print("Enter date'mm/dd/yyyy': ");
+			input = sc.next();
+			Date d = new Date();
+			try
+			{
+				@SuppressWarnings("deprecation")
+				long l = (long)Date.parse(input);
+				Date newD = new Date(l); 
+				System.out.println("-----------Duration --------");
+				System.out.println("In Days --> "+ (d.getDate() - newD.getDate()));
+				System.out.println("In Months -- >"+ (d.getMonth() - newD.getMonth()));
+				System.out.println("In years --> " +(d.getYear() - newD.getYear()));
+			}catch (Exception e) {
+				// TODO: handle exception
+				System.out.println("Enter date in format 'mm/dd/yyyy'");
+			}
+		}
+		//sc.close();
 	}
 
 }
